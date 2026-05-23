ae955d62a8272903f161a535bf005c70bd554d36
Valpal-49 <valliammaisaravanan.3101@gmail.com>
2026-05-23 14:09:34 +0530

    created main

---
+++ src/ext/Main.java
@@ -0,0 +1,10 @@
+package ext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello from Java!");
+        System.out.println("Git and Jenkins Demo Project");
+
+    }
+}
